8f98234d170ab1b5d144bbedf265234ba72d9421
REW1420 <werramos1420@gmail.com>
2022-10-19 16:52:20 -0500

    mew model 4

--- src/models/author.js
+++ src/models/author.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const authorSchema = new Schema({
-   
+   id:{
+        type: String,
+        trim: true,
+   },
     name:{
         type: String,
         trim: true,
