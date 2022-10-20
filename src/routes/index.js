789a1984b604317c9f9ef8e88c1ec4bc1858e2fc
REW1420 <werramos1420@gmail.com>
2022-10-19 19:18:30 -0500

    mew model 8

--- src/routes/index.js
+++ src/routes/index.js
@@ -17,45 +17,45 @@ router.get('/api/test', (req, res) => {
 //book
 router.get('/api/book', bookController.list);
 
-router.get('/api/book/:_id', bookController.show);
+router.get('/api/book/:id', bookController.show);
 
 router.post('/api/book', bookController.add);
 
-router.put('/api/book/:_id', bookController.update);
+router.put('/api/book/:id', bookController.update);
 
-router.delete('/api/book/:_id', bookController.delete);
+router.delete('/api/book/:id', bookController.delete);
 
 //registry
 router.get('/api/registry', RegistryController.list);
 
-router.get('/api/registry:_id', RegistryController.show);
+router.get('/api/registry:id', RegistryController.show);
 
 router.post('/api/registry', RegistryController.add);
 
-router.put('/api/registry/:_id', RegistryController.update);
+router.put('/api/registry/:id', RegistryController.update);
 
-router.delete('/api/registry/:_id', RegistryController.delete);
+router.delete('/api/registry/:id', RegistryController.delete);
 
 //wish book
 router.get('/api/wish', wishController.list);
 
-router.get('/api/wish/:_id', wishController.show);
+router.get('/api/wish/:id', wishController.show);
 
 router.post('/api/wish', wishController.add);
 
-router.put('/api/wish/:_id', wishController.update);
+router.put('/api/wish/:id', wishController.update);
 
-router.delete('/api/wish/:_id', wishController.delete);
+router.delete('/api/wish/:id', wishController.delete);
 
 //author
 router.get('/api/author', authorController.list);
 
-router.get('/api/author/:_id', authorController.show);
+router.get('/api/author/:id', authorController.show);
 
 router.post('/api/author', authorController.add);
 
-router.put('/api/author/:_id', authorController.update);
+router.put('/api/author/:id', authorController.update);
 
-router.delete('/api/author/:_id', authorController.delete);
+router.delete('/api/author/:id', authorController.delete);
 
 module.exports = router;
